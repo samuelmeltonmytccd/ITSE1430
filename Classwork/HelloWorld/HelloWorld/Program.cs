43dff4c36a92d1e6a29ec4011f3ea8231ce965e7
samuel.melton <samuel.melton@my.tccd.edu>
2019-01-16 21:43:39 -0600

    class complete 1-16-19

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*
+ * Multi line comment
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +9,18 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
+    // Single line comment
     class Program
     {
         static void Main( string[] args )
         {
-            Console.WriteLine("Hello World");
+            string name;
+
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Hello ");
+            Console.WriteLine(name);
         }
     }
 }
