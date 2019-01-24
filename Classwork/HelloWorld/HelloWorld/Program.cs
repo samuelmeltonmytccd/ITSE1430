913774dc535b05317600a3c7a564c6c44e087e45
samuel.melton <samuel.melton@my.tccd.edu>
2019-01-23 21:23:38 -0600

    1/23/19 done

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -14,13 +14,87 @@ namespace HelloWorld
     {
         static void Main( string[] args )
         {
-            string name;
+            NewGame();
+            DisplayGame();
+        }
 
-            Console.WriteLine("Enter your name: ");
+        private static void NewGame()
+        {
+            Console.WriteLine("Enter the name: ");
             name = Console.ReadLine();
 
-            Console.Write("Hello ");
-            Console.WriteLine(name);
+            owned = ReadBoolean("Owned? (Y/N)");
+
+            price = ReadDecimal("Price?");
+
+            Console.WriteLine("Publisher?");
+            publisher = Console.ReadLine();
+
+            completed = ReadBoolean("Completed? (Y/N)");
         }
+
+        private static void DisplayGame()
+        {
+
+            //string literal1 = "Hello Bob";
+            //string path = "C:\\Windows\\System32";
+            //string path2 = @"C:\Windows\System32";
+
+            Console.WriteLine("Name:\t" + name);
+            Console.WriteLine("Price:\t" + price);
+            Console.WriteLine("Publisher:\t" + publisher);
+            Console.WriteLine("Owned?\t" + owned);
+            Console.WriteLine("Completed?\t" + completed);
+
+        }
+
+        private static bool ReadBoolean ( string message)
+        {
+            Console.WriteLine(message);
+            string result = Console.ReadLine();
+
+            //Validate it is a boolean
+            //TODO: Fix this expression
+            if (result == "Y")
+                return true;
+            if (result == "y")
+                return true;
+            if (result == "N")
+                return false;
+            if (result == "n")
+                return false;
+
+            //TODO: Add validation
+            return false;
+        }
+
+        private static decimal ReadDecimal ( string message )
+        {
+            Console.WriteLine(message);
+            string value = Console.ReadLine();
+
+            if (Decimal.TryParse(value, out decimal result))
+                return result;
+
+            return 0;
+
+        }
+
+        private static string name;
+        private static string publisher;
+        private static decimal price;
+        private static bool owned;
+        private static bool completed;
+
     }
 }
+
+
+/*
+ * Video game class
+Name - string
+owned - bool
+price - decimal
+Publisher - string
+Completed? - bool
+*/
