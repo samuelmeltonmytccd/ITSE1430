dc3bfae71b95d6947dffafb46451f0b99f62c5e9
samuel.melton <samuel.melton@my.tccd.edu>
2019-01-28 21:35:34 -0600

    class work

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -36,48 +36,118 @@ namespace HelloWorld
         private static void DisplayGame()
         {
 
-            //string literal1 = "Hello Bob";
-            //string path = "C:\\Windows\\System32";
-            //string path2 = @"C:\Windows\System32";
+            //var literal1 = "Hello Bob";
+            //var path = "C:\\Windows\\System32";
+            //var path2 = @"C:\Windows\System32";
 
+            //var i = 10;
+
+            //1. String concat
             Console.WriteLine("Name:\t" + name);
-            Console.WriteLine("Price:\t" + price);
-            Console.WriteLine("Publisher:\t" + publisher);
+
+            //2. String Format
+            string str = String.Format("Price:\t {0:C}", price);
+            Console.WriteLine(str);
+            //Console.WriteLine("Price:\t" + price);
+
+            //3. Function overload - just calls string format
+            Console.WriteLine("Publisher:\t {0}", publisher);
+
+            //4. concatenation
+            //str = String.Concat("Owned?\t", " ", owned);
+            //Console.WriteLine(str);
             Console.WriteLine("Owned?\t" + owned);
-            Console.WriteLine("Completed?\t" + completed);
+
+            //5. Interpolation
+            Console.WriteLine($"Completed?\t {completed}");
+
+
+            /*
+            //Convert to a string
+            string strPrice = price.ToString("C");
+
+            //Is string empty
+            string input = "";
+            int length = input.Length;
+            bool isEmpty;
+
+            //if (input != null)
+            //    isEmpty = input.Length == 0;
+            //else
+            //    isEmpty = true;
+
+            isEmpty = (input != null) ? input.Length == 0 : true;
+
+            isEmpty = input.Length == 0;
+            isEmpty = input == "";
+            isEmpty = input == String.Empty;
+
+            isEmpty = String.IsNullOrEmpty(input);
+
+            //Comparison
+            bool areEqual = "Hello" == "hello";
+            areEqual = String.Compare("Hello", "hello", true) == 0;
+
+            //Conversion
+            input = input.ToUpper();
+            input = input.ToLower();
+
+            //Manipulation functions
+            bool startWith = input.StartsWith("http:");
+            bool endsWith = input.EndsWith("/");
+
+            input = input.TrimStart();
+            input = input.TrimEnd();
+            input = input.Trim();
+
+            input = input.PadLeft(10);
+            input = input.PadRight(10, '-');
+
+
+            */
 
         }
 
         private static bool ReadBoolean ( string message)
         {
-            Console.WriteLine(message);
-            string result = Console.ReadLine();
+            do
+            {
+                Console.WriteLine(message);
+                string result = Console.ReadLine().ToUpper();
 
-            //Validate it is a boolean
-            //TODO: Fix this expression
-            if (result == "Y")
-                return true;
-            if (result == "y")
-                return true;
-            if (result == "N")
-                return false;
-            if (result == "n")
-                return false;
+                //Validate it is a boolean
+                if (result == "Y")
+                    return true;
+                if (result == "N")
+                    return false;
+                /*
+                switch (result)
+                {
+                    case "Y":
+                    case "y": return true;
 
-            //TODO: Add validation
-            return false;
+                    case "N":
+                    case "n": return false;
+
+                    default: Console.WriteLine("Enter Y or N"); break;
+                }
+                */
+                Console.WriteLine("Enter Y or N");  
+            } while (true);
         }
 
         private static decimal ReadDecimal ( string message )
         {
-            Console.WriteLine(message);
-            string value = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine(message);
+                string value = Console.ReadLine();
 
-            if (Decimal.TryParse(value, out decimal result))
-                return result;
+                if (Decimal.TryParse(value, out decimal result))
+                    return result;
 
-            return 0;
-
+                Console.WriteLine("Not a valid entry.");
+            };
         }
 
         private static string name;
