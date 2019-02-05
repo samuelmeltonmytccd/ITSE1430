499549647a4c1a26498627a1fd1af7e92a6bd6fa
samuel.melton <samuel.melton@my.tccd.edu>
2019-02-04 21:37:26 -0600

    class 2/4/19

---
+++ Classwork/GameManager/GameManager.Host.Winforms/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameManager.Host.Winforms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            //accidentally changed Behavior>AutoValidate to its current setting 
+            InitializeComponent();
+
+            LoadUI();
+
+        }
+
+        void LoadUI()
+        {
+            Game game = new Game();
+
+            game.Completed
+            game.Name = "Dark Souls";
+            game.Price = 59.99M;
+            
+        }
+    }
+}
